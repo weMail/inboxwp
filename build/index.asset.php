34b6953ea1355d907d167478b552081056b98696
bdmehedi <bdmehedi92@gmail.com>
2023-02-16 00:09:27 +0600

    Production build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => '1ff8af9c6bf0713f076c');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => 'a71cad07b250306ad438');
