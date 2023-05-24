0cfdf383a2f68464129da1a0dfcd2e1c2e723b78
bdmehedi <bdmehedi92@gmail.com>
2023-05-24 19:35:39 +0600

    Added auto HRM

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const defaults = require('@wordpress/scripts/config/webpack.config');
+const path = require('path');
+
 
 module.exports = {
   ...defaults,
   externals: {
     react: 'React',
     'react-dom': 'ReactDOM',
+  },
+  devServer: {
+    hot: true, // Enable HMR
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    allowedHosts: 'auto',
+    host: 'appsero.test',
+    port: 8080,
+    proxy: {
+      '/build': {
+        pathRewrite: {
+          '^/build': '',
+        },
+      },
+    },
   },
 }; 
