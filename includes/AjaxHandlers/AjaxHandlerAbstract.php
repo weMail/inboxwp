0ca3d9e4575d780b3c66e5ec37e485bca484dc46
bdmehedi <bdmehedi92@gmail.com>
2023-07-20 17:48:25 +0600

    Remove allApi method from NonceChecker traits

--- includes/AjaxHandlers/AjaxHandlerAbstract.php
+++ includes/AjaxHandlers/AjaxHandlerAbstract.php
@@ -2,10 +2,31 @@
 
 namespace WeDevs\Inboxwp\AjaxHandlers;
 
+use WeDevs\Inboxwp\API\AppApi;
 use WeDevs\Inboxwp\Traits\NonceChecker;
 
 class AjaxHandlerAbstract {
 
     use NonceChecker;
+
+    /**
+     * Make api call
+     *
+     * @param $method
+     * @param $url
+     * @param $data
+     * @return void
+     */
+    protected function callApi($method, $url, $data = [])
+    {
+        $this->checkNonce();
+
+        $response = AppApi::instance()->{$method}($url, $data);
+
+        if (is_wp_error($response)) {
+            wp_send_json_error(['message' => $response->get_error_message()], $response->get_error_data()['status']);
+        }
+        wp_send_json_success($response);
+    }
 
 }
