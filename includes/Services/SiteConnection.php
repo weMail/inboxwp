856590af80ab203d4e3e8886fcc539e39bf9838d
bdmehedi <bdmehedi92@gmail.com>
2023-08-08 16:53:44 +0600

    Fixed admin url

--- includes/Services/SiteConnection.php
+++ includes/Services/SiteConnection.php
@@ -13,7 +13,7 @@ class SiteConnection {
      * @return string
      */
     public function appUrl() {
-        $redirect_url = admin_url() . '/admin.php?page=inboxwp';
+        $redirect_url = admin_url() . 'admin.php?page=inboxwp';
         $user = wp_get_current_user();
         $admin_name = $user->data->display_name;
         return INBOX_WP_APP_URL . '/checkout?rest_url_prefix=' . rest_get_url_prefix() . '&site_name=' . rawurlencode( get_bloginfo( 'name' ) ) . '&site_hash=' . inboxwp_site_hash() . '&home_url=' . untrailingslashit( home_url( '/' ) ) . '&site_url=' . untrailingslashit( site_url( '/' ) ) . '&site_email=' . get_bloginfo( 'admin_email' ) . '&admin_name=' . $admin_name . '&admin_email=' . $user->data->user_email . '&redirect_url=' . $redirect_url;
